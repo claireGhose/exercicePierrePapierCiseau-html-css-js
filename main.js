d367a8514d82a845554672a6f02ed9c5f8a3ab78
claire Ghose <clairedevillers@hotmail.fr>
2020-10-24 20:11:58 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let a= document.getElementById("btnpierre")
+    a.addEventListener ("click", function (event){
+
+    })
+
+    let b= document.getElementById("btnpapier")
+    b.addEventListener ("click", function (event){
+        
+    })
+
+
+    let c= document.getElementById("btnciseau")
+    c.addEventListener ("click", function (event){
+        
+    })
